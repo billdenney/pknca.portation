72a8b8900b091876a14091842b6223bc136dcd25
Bill Denney <wdenney@humanpredictions.com>
2017-11-25 23:02:02 -0500

    Document as.SDTM and update NAMESPACE

--- R/as.SDTM.R
+++ R/as.SDTM.R
@@ -1,14 +1,29 @@
+#' Convert an object into an SDTM object
+#'
+#' @param x The object to convert
+#' @return An object with the same class and classes of the SDTM domain
+#'   (like "SDTM_PC") and "SDTM" classes added.
+#' @export
 as.SDTM <- function(x, ...)
   UseMethod("as.SDTM")
 
+#' @rdname as.SDTM
+#' @export
 as.SDTM.data.frame <- function(x, ...) {
   detect_SDTM(x)
 }
 
+#' @rdname as.SDTM
+#' @export
 as.SDTM.tbl_df <- function(x, ...) {
   detect_SDTM(x)
 }
 
+#' Detect the SDTM domain for an object and add that class to the class
+#' of the object.
+#'
+#' @param x The object to detect using the "DOMAIN" column in the data
+#' @return The same object with the SDTM domain and SDTM classes added.
 detect_SDTM <- function(x) {
   if ("DOMAIN" %in% names(x)) {
     u_domain <- unique(x[["DOMAIN"]])
@@ -22,6 +37,15 @@ detect_SDTM <- function(x) {
   x
 }
 
+#' Calculate the nominal time from SDTM data
+#'
+#' @param x The object to extract the nominal time from
+#' @param col_day_suffix The suffix (after the domain) for the day
+#'   column (e.g. "PCDY")
+#' @param col_time_suffix The suffix (after the domain) for the nominal
+#'   time numeric column
+#' @return The vector of numeric times.
+#' @export
 sdtm_time_nominal <- function(x, col_day_suffix="DY", col_time_suffix="TPTNUM") {
   pattern_sdtm_domain <- "^SDTM_([A-Z]+)$"
   sdtm_domain <-
@@ -35,14 +59,26 @@ sdtm_time_nominal <- function(x, col_day_suffix="DY", col_time_suffix="TPTNUM") 
   (day - 1)*24 + time
 }
 
+#' @describeIn sdtm_time_nominal Calculate the nominal start time
+#' @export
 sdtm_time_start_nominal <- function(x, col_day_suffix="STDY", col_time_suffix="TPTNUM") {
   sdtm_time_nominal(x, col_day_suffix=col_day_suffix, col_time_suffix=col_time_suffix)
 }
+#' @describeIn sdtm_time_nominal Calculate the nominal end time
+#' @export
 sdtm_time_end_nominal <- function(x, col_day_suffix="ENDY", col_time_suffix="TPTNUM") {
   sdtm_time_nominal(x, col_day_suffix=col_day_suffix, col_time_suffix=col_time_suffix)
 }
 
-
+#' Convert an SDTM_PC object to a PKNCAconc object
+#' 
+#' @param x The SDTM object to convert
+#' @param ... arguments passed to PKNCAconc
+#' @param fun_time_nominal The function to calculate the nominal times.
+#' @param fun_time_actual The function to calculate the actual times.
+#' @return A PKNCAconc object
+#' @seealso \code{\link{as.PKNCAdose.SDTM_EX}}
+#' @export
 as.PKNCAconc.SDTM_PC <- function(x, ...,
                                  fun_time_nominal=sdtm_time_nominal,
                                  fun_time_actual=NULL) {
@@ -62,6 +98,17 @@ as.PKNCAconc.SDTM_PC <- function(x, ...,
             subject="USUBJID", exclude="exclude", ...)
 }
 
+#' Convert an SDTM_PC object to a PKNCAconc object
+#'
+#' @param x The SDTM object to convert
+#' @param ... arguments passed to PKNCAconc
+#' @param fun_time_start_nominal,fun_time_end_nominal The function to
+#'   calculate the nominal start and end times.
+#' @param fun_time_start_actual,fun_time_end_actual The function to
+#'   calculate the actual start and end times.
+#' @return A PKNCAdose object
+#' @seealso \code{\link{as.PKNCAconc.SDTM_PC}}
+#' @export
 as.PKNCAdose.SDTM_EX <- function(x, ...,
                                  fun_time_start_nominal=sdtm_time_start_nominal,
                                  fun_time_end_nominal=sdtm_time_end_nominal,
